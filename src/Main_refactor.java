06ead9cdcdaafd1b15193cec7587078d914efd51
Gabrivaa <gabriguepa@gmail.com>
2025-05-06 10:23:38 +0200

    Commit inicial

---
+++ src/Main_refactor.java
@@ -0,0 +1,4 @@
+import java.util.Scanner;
+
+public class Main_refactor implements Main_refactorizacion {
+}
---
+++ src/Main_refactorizacion.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public interface Main_refactorizacion {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hola mundo");
+    }
+}
